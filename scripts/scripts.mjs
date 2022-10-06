3a1990a7cf67a02ab3edd8ba36d8e71f67a22d58
Lewis Lovett <llovett7575@gmail.com>
2022-10-06 16:50:46 +0100

    Added code for word API

---
+++ scripts/scripts.mjs
@@ -0,0 +1,7 @@
+import fetch from 'node-fetch';
+
+
+const url = "https://random-word-api.herokuapp.com/word";
+// const response = ;
+// fetch(url).then(response => response.text);
+console.log(fetch(url).then(response => response.json));
