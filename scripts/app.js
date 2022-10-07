0bb6514c072f3dd1c8d311079030af26d48d0f10
Lewis Lovett <llovett7575@gmail.com>
2022-10-07 12:06:39 +0100

    Updated require call

--- scripts/app.js
+++ scripts/app.js
@@ -1,4 +1,5 @@
-const fetch = require('node-fetch');
+const moduleName = 'node-fetch';
+const fetch = require([moduleName]);
 const url = "https://random-word-api.herokuapp.com/word";
 // const response = ;
 // fetch(url).then(response => response.text);
