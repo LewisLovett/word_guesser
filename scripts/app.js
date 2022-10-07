42f477844db01bc9a912b576bac331c8deede619
Lewis Lovett <llovett7575@gmail.com>
2022-10-07 15:11:19 +0100

    API fetch now correctly gets a random word

--- scripts/app.js
+++ scripts/app.js
@@ -1,3 +1,12 @@
-  fetch("https://random-word-api.herokuapp.com/word")
-  .then((response) => response.json())
-  .then((data) => console.log(data));
+let newWord;
+const chooseWord = async (difficulty) => {
+    const url = `https://random-word-api.herokuapp.com/word?length=${difficulty}`;
+    const response = await fetch(url);
+    return response.json();
+}
+//    .then((response) => response.json()).then((data) => console.log(data[0]));
+const setWord = (word) => {
+    newWord = word;
+    document.querySelector(".scrambledWordDisplay").innerHTML = newWord;
+}
+chooseWord(4).then((data)=>{setWord(data[0])});
