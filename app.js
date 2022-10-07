56357244cc8d2145088363b69d707264febf707f
Lewis Lovett <llovett7575@gmail.com>
2022-10-07 11:44:07 +0100

    Switched to older fetch to be compatable with cjs

---
+++ app.js
@@ -0,0 +1,5 @@
+let fetch = require('node-fetch');
+const url = "https://random-word-api.herokuapp.com/word";
+// const response = ;
+// fetch(url).then(response => response.text);
+console.log(fetch(url).then(response => response.json));
--- scripts/scripts.js
+++
@@ -1,9 +0,0 @@
-
-import fetch from 'node-fetch';
-
-// const fetch = (...args) => import('node-fetch').then(({default: fetch}) => fetch(...args));
-
-const url = "https://random-word-api.herokuapp.com/word";
-// const response = ;
-// fetch(url).then(response => response.text);
-console.log(fetch(url).then(response => response.json));
