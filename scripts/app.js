706903b380aaf71554f5112d8c54a5c2d64e2304
Lewis Lovett <llovett7575@gmail.com>
2022-10-07 15:30:29 +0100

    Created scramble word function

--- scripts/app.js
+++ scripts/app.js
@@ -1,4 +1,6 @@
 let newWord;
+let scrambledWord;
+
 const chooseWord = async (difficulty) => {
     const url = `https://random-word-api.herokuapp.com/word?length=${difficulty}`;
     const response = await fetch(url);
@@ -7,6 +9,21 @@ const chooseWord = async (difficulty) => {
 //    .then((response) => response.json()).then((data) => console.log(data[0]));
 const setWord = (word) => {
     newWord = word;
-    document.querySelector(".scrambledWordDisplay").innerHTML = newWord;
+    scrambleWord(word);
 }
 chooseWord(4).then((data)=>{setWord(data[0])});
+
+const scrambleWord  = (newWord) =>{
+    let letterArray = newWord.split("");
+    let letterArrayLength = letterArray.length;
+
+    for (let i=0; i<letterArrayLength-1; i++){
+        const randNum = Math.floor(Math.random() *  letterArrayLength);
+
+        let temp = letterArray[i];
+        letterArray[i] = letterArray[randNum];
+        letterArray[randNum] = temp;
+    }
+    scrambledWord = letterArray.join("");
+    document.querySelector(".scrambledWordDisplay").innerHTML = scrambledWord;
+}
